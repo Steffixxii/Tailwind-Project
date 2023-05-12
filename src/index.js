2a543e005038e8a8263991991d21ec5f92b3e910
Estefanía García <egarcia@belcaire.local>
2023-05-12 11:37:24 +0200

    creando proyecto inicial

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDM1JQ2e3K6wXz7TcYNPAOXHHvXkxnoffY",
+  authDomain: "portal-de-empleados2.firebaseapp.com",
+  projectId: "portal-de-empleados2",
+  storageBucket: "portal-de-empleados2.appspot.com",
+  messagingSenderId: "945960943262",
+  appId: "1:945960943262:web:27e2f6737bae648d7fc58f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
